a913ecf2f852d8248dd76a051d806b17409e4e48
koba2264 <s2001k1022@gmail.com>
2025-07-03 14:44:47 +0900

    従業員のDAOとbeanを修正

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -23,16 +23,9 @@ public class EmployeeDAO extends DAO {
 			user.setNamef(rs.getString("NAMEF"));
 			user.setGender(rs.getString("GENDER_ID"));
 			user.setBirthDaySql(rs.getDate("BIRTHDAY"));
-			user.setAge(rs.getInt("AGE"));
-			user.setNumber(rs.getString("NUMBER"));
-			user.setBlood(rs.getString("BLOOD"));
-			user.setPhoto(rs.getString("PHOTO"));
-			user.setAddress(rs.getString("ADDRESS"));
 			user.setRole(rs.getString("ROLE"));
-			user.setSos1(rs.getString("SOS1"));
-			user.setSos2(rs.getString("SOS2"));
+			user.setJob(rs.getString("JOB_ID"));
 			user.setSpouse(rs.getBoolean("SPOUSE"));
-			user.setMeans(rs.getString("MEANS"));
 		}
 
 		ps.close();
@@ -45,23 +38,16 @@ public class EmployeeDAO extends DAO {
 	public boolean SignUp(Employee user, String password) throws Exception {
 		boolean result = false;
 		Connection con = getConnection();
-		PreparedStatement ps = con.prepareStatement("INSERT INTO EMPLOYEE VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);");
+		PreparedStatement ps = con.prepareStatement("INSERT INTO EMPLOYEE VALUES (?,?,?,?,?,?,?,?,?);");
 		ps.setString(1, user.getId());
 		ps.setString(2, user.getName());
 		ps.setString(3, user.getNamef());
 		ps.setString(4, password);
 		ps.setString(5, user.getGenderId());
 		ps.setDate(6, user.getBirthDaySql());
-		ps.setInt(7, user.getAge());
-		ps.setString(8, user.getNumber());
-		ps.setString(9, user.getBlood());
-		ps.setString(10, user.getPhoto());
-		ps.setString(11, user.getAddress());
-		ps.setString(12, user.getRole());
-		ps.setString(13, user.getSos1());
-		ps.setString(14, user.getSos2());
-		ps.setBoolean(15, user.isSpouse());
-		ps.setString(16, user.getMeansId());
+		ps.setString(7, user.getJobId());
+		ps.setString(8, user.getRole());
+		ps.setBoolean(9, user.isSpouse());
 		if (ps.executeUpdate() > 0) {
 			result = true;
 		}
